df83d5d1e7a598dc8f4435952285fb23c3cdd1b4
Andy <andy@Andys-MacBook-Pro.local>
2021-07-12 12:15:27 -0400

    Name.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ shortheading }}</h1>
+    <h2>{{ time }}</h2>
+    <ul v-if="items !== undefined">
+      <li v-for="(item, i) in items" :key="i">{{ item }}</li>
+    </ul>
+    <Name first="Andy" last="Li" />
+  </div>
+</template>
+
 <script lang="ts" setup>
 import { defineProps, computed, withDefaults } from 'vue';
 // import { stringRequired, stringOptional, arrayRequired } from '@/types/v-prop-types'
+import Name from './Name.vue'
 
 interface Props {
-  heading?: string,
-  time?: number,
-  items?: number[],
-  obj?: { foo: string },
+  heading: string
+  time?: number
+  items?: number[]
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  heading: '',
   time: 0,
   items: () => [1,2,3,4],
-  obj: () => ({ foo: 'x' })
 })
 
 // const props = Object.assign({}, _props, { items: [1,2,3] })
@@ -26,17 +35,6 @@ const props = withDefaults(defineProps<Props>(), {
 
 const shortheading = computed(() => props.heading.substr(0, 15))
 </script>
-
-<template>
-  <div class="hello">
-    <h1>{{ shortheading }}</h1>
-    <h2>{{ time }}</h2>
-    <ul v-if="items !== undefined">
-      <li v-for="(item, i) in items" :key="i">{{ item }}</li>
-    </ul>
-    <p>{{ props.obj.foo }}</p>
-  </div>
-</template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
---
+++ src/components/Name.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { withDefaults, defineProps } from 'vue'
+
+interface Name {
+  first: string
+  last?: string
+}
+
+const props = withDefaults(defineProps<Name>(), {
+  last: 'X'
+})
+</script>
+
+<template>
+  <div>
+    <p>{{ props.first }}</p>
+    <p>{{ props.last }}</p>
+  </div>
+</template>
+
+
